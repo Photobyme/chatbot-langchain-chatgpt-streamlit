a25e38bd8e5c33d7bcaef7c8acab6d1cdbd05440
kukjinkim <tlaruddo2@gmail.com>
2024-05-09 01:24:52 -0700

    feature: change ui like chat and prompt

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-# streamlit
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
-# api key for local
-# from dotenv import load_dotenv
-# load_dotenv()
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage, AIMessage
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
@@ -17,49 +10,81 @@ from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.memory import ChatMessageHistory
 import streamlit as st
 import requests
+import sys
+
+# only for deploy
+__import__('pysqlite3')
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
+# only for local develop
+# from dotenv import load_dotenv
+# load_dotenv()
 
 
-
-
-
-# Load the PDF file
+# load the PDF file from web
 pdf_url = "https://github.com/Photobyme/chatbot-langchain-chatgpt-streamlit/blob/main/photobyme-info.pdf?raw=true"
 response = requests.get(pdf_url)
 if response.status_code == 200:
     with open("photobyme-info.pdf", "wb") as file:
         file.write(response.content)
 
-# Initialize the chatbot and document processing
+#
+# initialize the chatbot and load data from PDF
 chat = ChatOpenAI(model="gpt-3.5-turbo-1106")
 loader = PyPDFLoader("photobyme-info.pdf")
 data = loader.load()
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
-all_splits = text_splitter.split_documents(data)
-vectorstore = Chroma.from_documents(documents=all_splits, embedding=OpenAIEmbeddings())
+
+# embegging
+textSplitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
+allSplits = textSplitter.split_documents(data)
+vectorstore = Chroma.from_documents(
+    documents=allSplits, embedding=OpenAIEmbeddings())
 retriever = vectorstore.as_retriever(k=100)
-docs = retriever.invoke("photo booth")
-question_answering_prompt = ChatPromptTemplate.from_messages(
+
+# retrive data from embedding
+docs = retriever.invoke("photobyme")
+questionAnsweringPrompt = ChatPromptTemplate.from_messages(
     [
         (
             "system",
-            "Answer the user's questions based on the below context:\n\n{context}",
+            "You're the Photobyme's customer service agent. Answer the customer's questions based on the below context:\n\n{context}",
         ),
         MessagesPlaceholder(variable_name="messages"),
     ]
 )
-document_chain = create_stuff_documents_chain(chat, question_answering_prompt)
-demo_ephemeral_chat_history = ChatMessageHistory()
+documentChain = create_stuff_documents_chain(chat, questionAnsweringPrompt)
+chatHistory = ChatMessageHistory()
 
 # Streamlit app
 st.title('Photobyme AI Chatbot')
-userInput = st.text_input("Have any questions?")
-if st.button("Please, answer"):
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+    st.session_state.messages.append(
+        {"role": "assistant", "content": "Hello 👋 How can I help you today? "})
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# React to user input
+if prompt := st.chat_input("Ask questions?"):
+    st.chat_message("user").markdown(prompt)
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
     with st.spinner("Wait for it..."):
-        demo_ephemeral_chat_history.add_user_message(userInput)
-        result = document_chain.invoke(
+        chatHistory.add_user_message(prompt)
+        result = documentChain.invoke(
             {
-                "messages": demo_ephemeral_chat_history.messages,
+                "messages": chatHistory.messages,
                 "context": docs,
             }
         )
-        st.write(result)
+
+    response = result
+    with st.chat_message("assistant"):
+        st.markdown(response)
+    st.session_state.messages.append(
+        {"role": "assistant", "content": response})
